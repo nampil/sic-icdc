e2d6d7f0b3cf8eff2fb41cfd915d96a3271f6c96
Nohel Madrid <nampil.dev@gmail.com>
2019-02-10 16:22:08 -0400

    history middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const graphQlSchema = require('./graphql/schema/index');
 const graphQLResolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
 const app = express();
+const history = require('connect-history-api-fallback');
 
 
 app.use(bodyParser.json());
@@ -22,12 +23,9 @@ app.use('/graphql', graphalHttp({
 
 app.set('port', process.env.PORT || 5000)
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-app.use(history({
-    disableDotRule: true,
-    verbose: true
-}));
+app.use(history());
 
 // mongoose.connect('mongodb://nampil:17abdi.sofi23@localhost:27017/icdcSicDb?retryWrites=true', {
 //         useNewUrlParser: true,
