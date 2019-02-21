9bd30d64f85228da4a0443d3e1bdb73f7e8ebe7f
Nohel Madrid <nampil.dev@gmail.com>
2019-02-21 02:02:03 -0400

    fixed url for api

--- graphql/resolvers/pupsub.js
+++ graphql/resolvers/pupsub.js
@@ -3,8 +3,8 @@ const {
 } = require('graphql-redis-subscriptions');
 const Redis = require('ioredis');
 const options = {
-    host: process.env.REDISCLOUD_URL,
-    port: process.env.REDISCLOUD_PORT,
+    host: 'redis-15418.c61.us-east-1-3.ec2.cloud.redislabs.com',
+    port: '15418',
     retry_strategy: options => {
         // reconnect after
         return Math.max(options.attempt * 100, 3000);
