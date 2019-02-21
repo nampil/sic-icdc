8cc0e9582cdcc130bad08395010742996a790b6d
Nohel Madrid <nampil.dev@gmail.com>
2019-02-21 02:16:45 -0400

    variables dotenv

--- graphql/resolvers/pupsub.js
+++ graphql/resolvers/pupsub.js
@@ -3,8 +3,9 @@ const {
 } = require('graphql-redis-subscriptions');
 const Redis = require('ioredis');
 const options = {
-    host: 'redis-15418.c61.us-east-1-3.ec2.cloud.redislabs.com',
-    port: '15418',
+    host: process.env.REDISCLOUD_URL,
+    password: process.env.REDISCLOUD_PASSWORD,
+    port: process.env.REDISCLOUD_PORT,
     retry_strategy: options => {
         // reconnect after
         return Math.max(options.attempt * 100, 3000);
